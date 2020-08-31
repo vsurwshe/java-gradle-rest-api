c6a42eb06ced2d78567c38ceba12f93c16e7b6db
amit patgaonkar <amit.patgaonkar@gmail.com>
2020-08-31 20:06:24 +0530

    src and bin deleted

--- src/main/java/com/rvtech/repository/ExpenceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rvtech.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rvtech.entity.ExpenceEntity;
-
-public interface ExpenceRepository extends CrudRepository<ExpenceEntity, String> {
-
-}
